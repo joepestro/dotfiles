8490a1d96aaced3192a9a1117b85432eb602e686
Joe Pestro <joepestro@gmail.com>
2012-01-26 15:21:19 -0800

    Titanium doesn't like to launch from cli anymore :(

--- titanium/aliases.zsh
+++ titanium/aliases.zsh
@@ -1,1 +1,1 @@
-alias titanium='/Users/joe/Library/Application\ Support/Titanium/mobilesdk/osx/1.6.2/iphone/builder.py "simulator" "4.3" . "com.joepestro.bargains" "Bargains"'
+alias titanium='/Users/joe/Library/Application\ Support/Titanium/mobilesdk/osx/1.8.0.1/iphone/builder.py "simulator" "4.3" . "com.joepestro.bargains" "Bargains"'
